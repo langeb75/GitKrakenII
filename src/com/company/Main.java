8980d43530d7e7c049b53894eb52382d08b3def5
Brad Lange <blange@gulllakecs.org>
2019-11-14 09:31:36 -0500

    Add Inner & Outer For Loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // This is my Inner and Outer FOr Loop
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Outer Loop: " + i);
+            for (int j = 0; j < 5; j++) {
+                System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will tab for me
+            }
+        }
     }
 }
+
