774afdf424c0d87a199ba5a26f8da446205e8221
Lange <bradlange75@gmail.com>
2019-11-14 10:45:47 -0500

    Add calculateAverage() Function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,17 +28,36 @@ public class Main {
         }
 
     }
-
+    
+    public static double calculateAverage(double [] temperature){
+    int size = temperature.length;
+    double total = 0;
+    for(int i=0; i<size; i++){
+        total += temperature[i];
+    }
+    double average = total/size;
+    System.out.println("Average Temperature of the " + size + " readings: " + Math.round(average * 100.0) / 100.0);
+    return average;
+        
+        
+    
+    
     public static void main(String[] args) {
-        // This is my Inner and Outer FOr Loop
+        //Inner and Outer For Loop
         for (int i = 0; i < 3; i++) {
             System.out.println("Outer Loop: " + i);
             for (int j = 0; j < 5; j++) {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will tab for me
             }
         }
-
+        //Call rollASix() Funnction
         rollASix();
+        
+        //Make an array of temperatures & calculate them after by calling the calculateAverage function
+        double [] temperature = {75, 54.48, 76, 89, 56, 78.9};
+    calculateAverage(temperature);
+        
+        //Call countBlocks() Function
         countBlocks(7);
     }
 
