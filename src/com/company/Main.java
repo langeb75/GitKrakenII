75a31ea22fd768e44400acd3f443349591fd2342
Lange <bradlange75@gmail.com>
2019-11-14 09:44:36 -0500

    Add rollASix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class Main {
+    public static void rollASix() {
+        int rollDice = 0;
+
+        while (rollDice != 6) {
+            rollDice = (int) (Math.random() * 6 + 1);
+            System.out.println("You rolled: " + rollDice);
+
+            if (rollDice == 3) {
+                break;
+            }
+        }
+        if (rollDice == 6) {
+            System.out.println("You Win!!!");
+        } else {
+            System.out.println("You Lose");
+        }
+    }
 
     public static void main(String[] args) {
         // This is my Inner and Outer FOr Loop
@@ -10,6 +27,7 @@ public class Main {
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!"); // \t will tab for me
             }
         }
+        rollASix();
     }
 }
 
